8ec2df2e58329b23d6899c55099a5db13d7cdb92
HsoubAcademy <academy@hsoub.co>
2023-06-12 09:50:25 +0300

    product page done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-
-window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle');
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 import '@fortawesome/fontawesome-free/js/all';
+
+
+
 console.log("hello, everyone");
 
 
@@ -21,3 +23,29 @@ document.querySelectorAll('.add-to-cart-btn').forEach(item =>{
 
 
 document.getElementById('copyright').innerHTML = 'جميع الحقوق محفوظة'+ new Date().getFullYear();
+
+
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+
+item.addEventListener("change", () => {
+  document.querySelectorAll('.size-option').forEach(i => {
+        i.classList.remove('active')
+  })
+  item.parentNode.parentNode.classList.add("active")
+
+})
+
+})
+
+
+document.querySelectorAll('.kind-option input[type="radio"]').forEach(item => {
+
+  item.addEventListener("change", () => {
+    document.querySelectorAll('.kind-option').forEach(i => {
+          i.classList.remove('active')
+    })
+    item.parentNode.parentNode.classList.add("active")
+  
+  })
+  
+  })
