d65ea1a72a965fe051f4174bdb83b87c0a3c3272
HsoubAcademy <academy@hsoub.co>
2023-06-14 20:47:08 +0300

    checkout page done

--- src/index.js
+++ src/index.js
@@ -49,3 +49,46 @@ document.querySelectorAll('.kind-option input[type="radio"]').forEach(item => {
   })
   
   })
+
+
+  document.querySelectorAll('.qunatity').forEach( item => {
+        item.addEventListener('change', () =>{
+           const newQuantity = item.value
+           const parent = item.closest('[data-product-info]')
+           const pricePerUbit = parent.getAttribute('data-product-price')
+           const totalPriceForProduct = newQuantity * pricePerUbit
+           parent.querySelector(".total-price-for-prodcut").innerHTML = totalPriceForProduct + '$';
+            
+           let totelPriceForAllProduct = 0;
+           document.querySelectorAll('[data-product-info]').forEach(product =>{
+                const priceForUnite = product.getAttribute('data-product-price')
+                const QuantityAll = product.querySelector ('.qunatity').value
+                const totalPriceForAllProduct1 = priceForUnite * QuantityAll
+
+                totelPriceForAllProduct = totelPriceForAllProduct + totalPriceForAllProduct1;
+           })
+            document.getElementById('price-for-all-products').innerHTML = totelPriceForAllProduct + '$'
+        })
+  })
+
+  document.querySelectorAll('[data-remove-from-card]').forEach( item => {
+
+  item.addEventListener('click', () => {
+      item.closest('[data-product-info]').remove()
+
+      calculateTotalPrice()
+  })
+
+  })
+
+  function calculateTotalPrice(){
+    let totelPriceForAllProduct = 0;
+    document.querySelectorAll('[data-product-info]').forEach(product =>{
+         const priceForUnite = product.getAttribute('data-product-price')
+         const QuantityAll = product.querySelector ('.qunatity').value
+         const totalPriceForAllProduct1 = priceForUnite * QuantityAll
+
+         totelPriceForAllProduct = totelPriceForAllProduct + totalPriceForAllProduct1;
+    })
+     document.getElementById('price-for-all-products').innerHTML = totelPriceForAllProduct + '$'
+  }
